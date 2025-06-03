26d251aade2b4e74b40a9b20ad559a6913b2a92d
Davy <davyns011@gmail.com>
2025-06-03 20:28:41 -0300

    1000 para 100

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,7 +1,8 @@
 let listaDeNuerosSorteados = []
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
+console.log(numeroSecreto);
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -18,7 +19,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagensIniciais() {  
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'escolha um número entre 1 e 100');
 }
 
 exibirMensagensIniciais();
